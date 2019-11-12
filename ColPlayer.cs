d67782b0ed19bd8b29d7a1d707e84cbfa1026c24
C-121 <56976403+C-121@users.noreply.github.com>
2019-11-12 13:28:37 -0600

    Add files via upload

--- ColPlayer.cs
+++ ColPlayer.cs
@@ -1,11 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ColPlayer : MonoBehaviour
-{
+{	
+	public Text texto;
 	public float vida;
 	public float dano;
+	public int monedas;
+
 
 	private Mover mover;
 
@@ -16,6 +20,10 @@ public class ColPlayer : MonoBehaviour
     void OnTriggerEnter2D(Collider2D col){
     	if(col.CompareTag("ZonaDano") && !mover.isJumping){
     		vida-=dano*Random.Range(0.5f,5f);
+    	}
+    	if(col.CompareTag("Moneda")){
+    		monedas = monedas + 1;
+    		texto.text = "Monedas: " + monedas;
     	}
     }
     void OnTriggerStay2D(Collider2D col){
