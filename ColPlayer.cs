2c8087397c9c71f68a42e058a2d7014af6db7034
C-121 <56976403+C-121@users.noreply.github.com>
2019-11-14 13:36:56 -0600

    Add files via upload

--- ColPlayer.cs
+++ ColPlayer.cs
@@ -6,20 +6,27 @@ using UnityEngine.UI;
 public class ColPlayer : MonoBehaviour
 {	
 	public Text texto;
+	public Text textoVida;
 	public float vida;
 	public float dano;
 	public int monedas;
+	public float tiempoSeg;
 
 
 	private Mover mover;
 
 	void Start(){
 		mover = GetComponent<Mover>();
+		tiempoSeg = 0;
 	}
 	
+	void Update(){
+		tiempoSeg += Time.deltaTime;
+	}
     void OnTriggerEnter2D(Collider2D col){
     	if(col.CompareTag("ZonaDano") && !mover.isJumping){
     		vida-=dano*Random.Range(0.5f,5f);
+    		textoVida.text = "Salud: " + Mathf.Round(vida);
     	}
     	if(col.CompareTag("Moneda")){
     		monedas = monedas + 1;
@@ -29,6 +36,7 @@ public class ColPlayer : MonoBehaviour
     void OnTriggerStay2D(Collider2D col){
     	if(col.CompareTag("ZonaDano") && !mover.isJumping){
     		vida-=dano*Time.deltaTime;
+    		textoVida.text = "Salud: " + Mathf.Round(vida);
     	}
     }
 }
---
+++ Final.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Final : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public ColPlayer jugador;
+    public int MonedasParaGanar;
+    public float tiempoParaPerder;
+   	public Text textoTiempo;
+    public GameObject imagenVictoria;
+    public GameObject imagenDerrota;
+    public GameObject imagenDerrotaTiempo;
+
+
+    void Start()
+    {        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	textoTiempo.text = Mathf.Round(tiempoParaPerder - jugador.tiempoSeg) + "";
+    	if(jugador.monedas >= MonedasParaGanar){
+    		imagenVictoria.SetActive(true);
+    		Invoke("CargarEscena", 2f);
+    	}    
+    	if(jugador.vida <= 0){
+    		imagenDerrota.SetActive(true);
+    		Invoke("CargarEscena", 2f);
+    	}
+    	if(jugador.tiempoSeg > tiempoParaPerder){
+    		imagenDerrotaTiempo.SetActive(true);
+    		Invoke("CargarEscena", 2f);	
+    	}
+    }
+
+    void CargarEscena(){
+    	SceneManager.LoadScene("SampleScene");
+    }
+}
